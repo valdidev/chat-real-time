eee41d636955ded663cefd071470a9a0ddad3eb8
valdidev <valdicode.fernando@gmail.com>
2022-12-10 20:46:03 +0100

    prepare for deploy

--- server/index.js
+++ server/index.js
@@ -4,8 +4,12 @@ import { Server as SockerServer } from 'socket.io';
 import http from 'http';
 import cors from 'cors';
 import { PORT } from './config.js';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url));
+console.log(__dirname);
 const server = http.createServer(app);
 const io = new SockerServer(server, {
     cors: {
@@ -27,5 +31,7 @@ io.on('connection', (socket) => {
         });
     });
 });
+
+app.use(express.static(join(__dirname, '../client/dist')));
 
 server.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
