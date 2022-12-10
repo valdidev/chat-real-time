1b8ad3e1db8ebefe1deee04c14055eb213b1d67f
valdidev <valdicode.fernando@gmail.com>
2022-12-10 20:24:53 +0100

    ui messages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,71 @@
-import './App.css';
-import io  from 'socket.io-client';
-import { useState } from 'react';
+import "./App.css";
+import io from "socket.io-client";
+import { useState, useEffect } from "react";
 
-const socket = io('http://localhost:4000');
+const socket = io("http://localhost:4000");
 
 function App() {
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([
+    {
+      body: "Welcome",
+      from: "admin",
+    },
+  ]);
 
-  const [message, setMessage] = useState('');
+  useEffect(() => {
+    const receivedMessage = (message) => {
+      setMessages([message, ...messages]);
+      console.log(messages);
+    };
+
+    socket.on("message", receivedMessage);
+
+    return () => {
+      socket.off("message", receivedMessage);
+    };
+  }, [messages]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    socket.emit('message', message);
-  }
+    socket.emit("message", message);
+    const newMessage = {
+      body: message,
+      from: "Me",
+    };
+    setMessages([newMessage, ...messages]);
+    setMessage("");
+  };
 
   return (
-    <div className="App">
-      <form onSubmit={handleSubmit}>
-        <input type="text" onChange={e => setMessage(e.target.value)}/>
-        <button>Send</button>
+    <div className="h-screen bg-zinc-800 text-white flex items-center justify-center">
+      <form onSubmit={handleSubmit} className="bg-zinc-900 p-10">
+        <h1 className="text-2xl font-bold my-2 text-center">Chat React</h1>
+        <input
+          type="text"
+          onChange={(e) => setMessage(e.target.value)}
+          value={message}
+          className="border-2 border-zinc-500 p-2 text-black w-full"
+        />
+        <button className="bg-green-700 p-2 w-full py-1 my-1">Send</button>
+
+        <ul className="h-80 overflow-y-auto">
+          {messages.map((message, index) => (
+            <li
+              key={index}
+              className={`my-2 p-2 table text-sm rounded-md ${
+                message.from === "Me" ? "bg-sky-700 ml-auto" : "bg-black"
+              }`}
+            >
+              <p>
+                {message.from}: {message.body}
+              </p>
+            </li>
+          ))}
+        </ul>
       </form>
     </div>
   );
-};
+}
 
 export default App;
